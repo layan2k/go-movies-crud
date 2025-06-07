eaf7ad24540b7d7447810c483a719af1f452c9da
layan2k <lesliedeeshumba@gmail.com>
2025-06-07 13:45:11 +0200

    Fix content type headers and implement movie update functionality

--- main.go
+++ main.go
@@ -11,23 +11,56 @@ import (
 )
 
 func getMovies(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "applicat/json")
+	json.NewEncoder(w).Encode(movies)
 }
 
 func getMovie(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range movies {
+		if item.ID == params["id"]{
+			json.NewEncoder(w).Encode(item)
+		}
+	}
 }
 
 func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(100000000))
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
 
 }
 
 func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index, item := range movies{
+		if item.ID == params["id"]{
+			movies = append(movies[:index], movies[index + 1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+		}
+	}
 
 }
 
 func deleteMovie(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type","application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"]{
+			movies = append(movies[:index], movies[index + 1:]...)
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
 }
 
 type Movie struct {
@@ -58,5 +91,4 @@ func main() {
 
 	fmt.Printf("Starting server at port %d", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), r))
-
 }
